55794f2bbcd4604d5ef9d0a4432f5b0c2c4ec4ea
FamALouiz <fam@awadlouis.com>
2025-04-09 15:10:29 +0200

    Added join path util file

---
+++ libs/util/path.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifdef _WIN32
+#define PATH_SEPARATOR "\\"
+#else
+#define PATH_SEPARATOR "/"
+#endif
+
+void join_paths(char *result, int num_paths, const char *paths[])
+{
+    result[0] = '\0'; // Initialize result as an empty string
+
+    for (int i = 0; i < num_paths; i++)
+    {
+        if (i > 0)
+        {
+            size_t len = strlen(result);
+            if (len > 0 && result[len - 1] != PATH_SEPARATOR[0])
+            {
+                strcat(result, PATH_SEPARATOR);
+            }
+        }
+        strcat(result, paths[i]);
+    }
+}
