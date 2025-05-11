63bc3135d0cf938b5770752938c9c4230c94d10f
Amiir-10 <amirhany2004@gmail.com>
2025-05-11 23:45:37 +0300

    imported needed library for stnscpy to work and opened file for preParser

--- libs/parser/parser.c
+++ libs/parser/parser.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>  
+#include <string.h> 
 #include <mem_manager.h>
 #include <word.h>
 #include <logger.h>
---
+++ libs/parser/preParser.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <mem_manager.h>
+#include <word.h>
+#include <logger.h>
+
+void preParse(word* word){//this is for iterating over the word to convert it to bits
+
+
+    char binary[32];
+    
+}
