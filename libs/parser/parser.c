838b3c8e96fe7e91568fca0b1311752baf416eb3
FamALouiz <fam@awadlouis.com>
2025-05-22 11:05:25 +0200

    Updated parser to return 1 when ending

--- libs/parser/parser.c
+++ libs/parser/parser.c
@@ -52,7 +52,7 @@ int binToDec(char bin[])
     }
     return res;
 }
-void parse(word *word)
+int parse(word *word)
 {
     char *inst = preParse(word);
     char opCode[4];
@@ -145,6 +145,7 @@ void parse(word *word)
     int LSRInt = binToDec(LSR);
     int MOVRInt = binToDec(MOVR);
     int MOVMInt = binToDec(MOVM);
+    int ENDInt = 0xFFFFFFFF;
     free(ADD);
     free(SUB);
     free(MUL);
@@ -157,6 +158,11 @@ void parse(word *word)
     free(LSR);
     free(MOVR);
     free(MOVM);
+
+    if (opCodeInt == ENDInt)
+    {
+        return 1;
+    }
 
     if (opCodeInt == ADDInt)
     {
